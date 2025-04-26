8403fa5c2dd7be96f5953438dfffab6b9acf3afe
cubi4 <abrau2012@web.de>
2025-04-27 01:05:16 +0200

    added: first UI Parts (TaskCard)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 import { useEffect, useState } from "react";
-function MyButton() {
-    return (
-        <button className="bg-red-500 text-white hover:bg-sky-700 rounded ">
-            Click Me!
-        </button>
-    );
-}
+
 
 function App() {
     const [message, setMessage] = useState<string>("");
@@ -23,9 +19,24 @@ function App() {
     return (
         <div className="flex justify-center items-center flex-col min-h-svh">
             <h1 className="text-4xl">Message from Backend:</h1>
-            <p className="text-white">{message}</p>
-            <MyButton />
-            <Button>This is a UI-Component</Button>
+            <p className="text-purple">{message}</p>
+            {/* Timer Card */}
+            <Card className="w-full max-w-2xl shadow-md">
+                <CardHeader>
+                    <CardTitle className="text-2xl flex items-center gap-2">
+                        Task Name
+                    </CardTitle>
+                </CardHeader>
+                
+                <CardContent className="flex flex-col gap-6">
+                    {/* Task Name Eingabefeld */}
+                    <Input placeholder="Task Name" />
+                    {/* Timer Anzeige */}
+                    <div className="text-4xl font-bold text-center">00:00</div>
+                    {/* Start Button */}
+                    <Button className="w-full">Start</Button>
+                </CardContent>
+            </Card>
         </div>
     );
 }
